dd19989381e851c3447cb6a90f99c2b88ad69c9a
AntonGrigorev <antongr2001@gmail.com>
2020-06-04 16:54:45 +0300

    message

---
+++ tests/test2.cpp
@@ -0,0 +1,27 @@
+#include <Account.h>
+#include <Transaction.h>
+#include <gmock/gmock.h>
+#include <gtest/gtest.h>
+
+inline bool operator==(Account a, Account b) { return true; }
+
+class MockAccount: public Account{
+  public:
+  MockAccount(int id, int balance):Account(id, balance){}
+  MOCK_METHOD(int, GetBalance, (), (const, override));
+  MOCK_METHOD(void, ChangeBalance, (int diff), (override));
+  MOCK_METHOD(void, Lock, (), (override));
+  MOCK_METHOD(void, Unlock, (), (override));
+};
+
+class MockTransaction: public Transaction{
+  public:
+  MOCK_METHOD(void, SaveToDataBase, (Account& from, Account& to, int sum), (override));
+};
+
+TEST(Transaction, SaveToDataBase) {
+  MockTransaction tr;
+  MockAccount from(1, 500);
+  MockAccount to(2, 400);
+  EXPECT_CALL(tr, SaveToDataBase(from, to, 150)).Times(1);
+}
